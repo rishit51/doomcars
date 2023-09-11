43c628fb5cb44e8a85f7ab6d2f094622eb6184cd
Rishit Srivastava <101878136+rishit51@users.noreply.github.com>
2023-09-11 07:06:34 +0000

    test-commit2

--- doomcars/api/models.py
+++ doomcars/api/models.py
@@ -1,3 +1,6 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+import uuid
+# Create your models here.
+class Car(models.Model):
+    id=models.UUIDField(default=uuid.uuid3,primary_key=True,unique=True,blank=False)
